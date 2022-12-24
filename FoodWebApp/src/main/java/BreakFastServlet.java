357aeb9863b55f472ccadfb273063054218608ae
KHADRI SOFTWARE INSTITUTION <86885935+khadri2021@users.noreply.github.com>
2022-12-24 17:29:06 +0530

    initial setup of project
    by khadri

---
+++ FoodWebApp/src/main/java/BreakFastServlet.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class BreakFastServlet extends HttpServlet {
+
+	@Override
+	protected void service(HttpServletRequest arg0, HttpServletResponse arg1) throws ServletException, IOException {
+
+		System.out.println("############ Idly....############");
+		System.out.println("############ Idly....############");
+		System.out.println("############ Idly....############");
+		System.out.println("############ Idly....############");
+
+	}
+}
---
+++ FoodWebApp/src/main/java/LunchServlet.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LunchServlet extends HttpServlet {
+
+	@Override
+	protected void service(HttpServletRequest arg0, HttpServletResponse arg1) throws ServletException, IOException {
+
+		System.out.println("############ Veg-Meals....############");
+		System.out.println("############ Veg-Meals....############");
+		System.out.println("############ Veg-Meals....############");
+		System.out.println("############ Veg-Meals....############");
+
+	}
+}
