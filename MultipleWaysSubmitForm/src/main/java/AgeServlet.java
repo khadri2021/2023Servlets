18449b9a29627facefc7761a5f29c4ab802aec1c
KHADRI SOFTWARE INSTITUTION <86885935+khadri2021@users.noreply.github.com>
2022-12-27 18:16:24 +0530

    example project

---
+++ MultipleWaysSubmitForm/src/main/java/AgeServlet.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AgeServlet extends HttpServlet{
+
+	
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		System.out.println(request.getParameter("age"));
+		System.out.println(request.getParameter("phone"));
+	
+	}
+}
