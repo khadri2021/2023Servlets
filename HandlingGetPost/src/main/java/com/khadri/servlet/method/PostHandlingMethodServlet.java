0c3133c3571761d92333e02410209c07b9ca6734
KHADRI SOFTWARE INSTITUTION <86885935+khadri2021@users.noreply.github.com>
2022-12-28 08:25:40 +0530

    Handling Get and Post

---
+++ HandlingGetPost/src/main/java/com/khadri/servlet/method/PostHandlingMethodServlet.java
@@ -0,0 +1,19 @@
+package com.khadri.servlet.method;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class PostHandlingMethodServlet extends HttpServlet {
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	System.out.println("PostHandlingMethodServlet Invoked!!!!");
+	
+	}
+	 
+
+}
