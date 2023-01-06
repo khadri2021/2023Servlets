c65086f4691752da4a6d6ca66a93013ea8c196fc
KHADRI SOFTWARE INSTITUTION <86885935+khadri2021@users.noreply.github.com>
2023-01-06 09:30:42 +0530

    initial commits

---
+++ ServletFilter/src/main/java/com/khadri/servlet/TestServlet.java
@@ -0,0 +1,28 @@
+package com.khadri.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class TestServlet extends HttpServlet {
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("TestServlet Invoked");
+		
+		PrintWriter writer = resp.getWriter();
+		writer.println("<html>");
+		writer.println("<body bgcolor='green'>");
+		writer.println("<center>");
+		writer.println("Hurry successfuly logged in !!!!!");
+		writer.println("</center>");
+		writer.println("</body>");
+		writer.println("</html>");
+		writer.close();
+
+	}
+}
